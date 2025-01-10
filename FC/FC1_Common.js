244daf33a4d4370f1f5a3a97656ff50170e6c17b
vangng57 <nguyenvang5702@gmail.com>
2025-01-10 11:25:44 +0700

    add IO

--- FC/FC1_Common.js
+++ FC/FC1_Common.js
@@ -1,6 +1,6 @@
 // Hàm chức năng ghi giá trị tag vào DB_Input
 function setTag(tag,val) {
-    var tag_link = '"DB_Input."' + tag;
+    var tag_link = '"DB_Input".' + tag;
     var url = "IO.html";
     sdata =  tag_link  + '=' + val;
     $.post(url, sdata, function (result2) { });
@@ -14,22 +14,22 @@ function IOField(ObjectID, tag){
 }
 // Hiển thị giá trị lên object id
 setInterval(function(){
-    if (tag_Edit_enable == true){
-    IOField("tbx_may_tron","DB_Output.tag1");
+    if (tag_Edit_enable == false){
+    IOField("tbx_may_tron","Tron_DI");
     }
 },1000);
 // Ham chuc nang nut sua / luu du lieu
 function fn_DataEdit(button1, button2)
 {
-    document.getElementById(button1).style.zIndex = "1";
-    document.getElementById(button2).style.zIndex = "0";
+    document.getElementById(button1).style.zIndex = "1"; // hien nut 1
+    document.getElementById(button2).style.zIndex = "0"; // an nut 2
 }
 //Tag Edit
 var tag_Edit_enable = false;
 //Ham bao dang sua du lieu
 function fn_Editing()
 {
-    fn_DataEdit("btn_Save","btn_Edit");
+    fn_DataEdit("btt_Save","btt_Edit");
     tag_Edit_enable = true;
     //
     document.getElementById("tbx_may_tron").disabled = false;
@@ -38,12 +38,12 @@ function fn_Editing()
 //Ham bao luu du lieu
 function fn_Saving()
 {
-    fn_DataEdit("btn_Edit","btn_Save");
+    fn_DataEdit("btt_Edit","btt_Save");
     tag_Edit_enable = false;
     //Lay gia tri tu textbox
     var tag_data = document.getElementById("tbx_may_tron").value;
 
-    setTag('ten_tag',tag_data);
+    setTag('Tron_DI',tag_data);
     //Disable chuc nang IO field
     document.getElementById("tbx_may_tron").disabled = true;
     alert("Lưu dữ liệu thành công");
